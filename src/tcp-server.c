bee15511ab92a5bfcfd0e5f5f4f5b1122b57016f
kyechou <kuanyenchou@gmail.com>
2018-12-11 14:21:36 -0600

    Format outputs and prepare errqueue

--- src/tcp-server.c
+++ src/tcp-server.c
@@ -9,6 +9,7 @@
 #include <errno.h>
 #include <unistd.h>
 #include <linux/net_tstamp.h>
+#include <linux/errqueue.h>
 #include <sys/time.h>
 
 const char usage[] = "Usage: ./echo-server <port>\n";
@@ -62,37 +63,93 @@ static void reaper(int sig)
 	signal(sig, reaper);
 }
 
-static void printstamps(struct msghdr *msg)
+static inline void __print_ts(struct scm_timestamping *tss,
+		struct sock_extended_err *serr)
 {
+	int type, id;
+	char name[64];
 	struct timespec *ts;
+
+	if (serr) {
+		type = serr->ee_info;
+		id = serr->ee_data;
+	} else {
+		type = id = -1;
+	}
+	switch(type) {
+	case -1:
+		strncpy(name, "RX    ", sizeof(name));
+		break;
+	case SCM_TSTAMP_SND:
+		strncpy(name, "TX    ", sizeof(name));
+		break;
+	case SCM_TSTAMP_SCHED:
+		strncpy(name, "SCHED ", sizeof(name));
+		break;
+	case SCM_TSTAMP_ACK:
+		strncpy(name, "ACK   ", sizeof(name));
+		break;
+	default:
+		strncpy(name, "??    ", sizeof(name));
+	}
+
+	for (int i = 0; i < 3; i += 2) {
+		switch(i) {
+		case 0:
+			strncat(name, "SW", sizeof(name) - strlen(name) - 1);
+			break;
+		case 2:
+			strncat(name, "HW", sizeof(name) - strlen(name) - 1);
+			break;
+		}
+
+		ts = &tss->ts[i];
+		if (ts->tv_sec == 0 && ts->tv_nsec == 0)
+			continue;
+		fprintf(stderr, "%s: [%3d] %lu.%09lu\n", name, id, ts->tv_sec,
+				ts->tv_nsec);
+	}
+}
+
+static inline void printstamps(struct msghdr *msg)
+{
+	struct sock_extended_err *serr = NULL;
+	struct scm_timestamping *tss = NULL;
 	struct cmsghdr *cm;
+	int batch = 0;
 
 	for (cm = CMSG_FIRSTHDR(msg); cm; cm = CMSG_NXTHDR(msg, cm)) {
-		if (cm->cmsg_level != SOL_SOCKET) {
-			fprintf(stderr, "unsupported level %d\n",
-					cm->cmsg_level);
-			continue;
+		if (cm->cmsg_level == SOL_SOCKET &&
+		                cm->cmsg_type == SCM_TIMESTAMPING) {
+			tss = (struct scm_timestamping *) CMSG_DATA(cm);
+		} else if ((cm->cmsg_level == SOL_IP &&
+		                cm->cmsg_type == IP_RECVERR) ||
+		                (cm->cmsg_level == SOL_IPV6 &&
+		                 cm->cmsg_type == IPV6_RECVERR)) {
+			serr = (struct sock_extended_err *) CMSG_DATA(cm);
+			if (serr->ee_errno != ENOMSG || serr->ee_origin !=
+					SO_EE_ORIGIN_TIMESTAMPING) {
+				fprintf(stderr, "unsupported ip error: %d, "
+						"%d\n", serr->ee_errno,
+						serr->ee_origin);
+				serr = NULL;
+			}
+		} else {
+			fprintf(stderr, "unsupported cmsg level, type: %d, "
+			        "%d\n", cm->cmsg_level, cm->cmsg_type);
 		}
-		switch(cm->cmsg_type) {
-		case SO_TIMESTAMP:
-			break;
-		case SO_TIMESTAMPNS:
-			break;
-		case SO_TIMESTAMPING:
-			ts = (struct timespec *)CMSG_DATA(cm);
-			fprintf(stderr, "%ld.%09ld:\tSW\n", (long)ts->tv_sec,
-					(long)ts->tv_nsec);
-			ts++;
-			ts++; /* skip deprecated HW transformed */
-			fprintf(stderr, "%ld.%09ld:\tHW\n", (long)ts->tv_sec,
-					(long)ts->tv_nsec);
-			break;
-		default:
-			fprintf(stderr, "unsupported type %d\n",
-					cm->cmsg_type);
-			break;
+
+		/* print the timestamps */
+		if (tss) {
+			__print_ts(tss, serr);
+			tss = NULL;
+			serr = NULL;
+			++batch;
 		}
 	}
+
+	if (batch > 1)
+		fprintf(stderr, "batched %d timestamps\n", batch);
 }
 
 #define BUF_SZ 1024
@@ -118,16 +175,13 @@ static int my_recv(int fd, void *buf, size_t len)
 	iov.iov_base = buf;
 	iov.iov_len = len;
 
-	ret = recvmsg(fd, &msg, MSG_ERRQUEUE);
+	//ret = recvmsg(fd, &msg, MSG_ERRQUEUE);
+	ret = recvmsg(fd, &msg, 0);
 	if (ret < 0 && errno != EAGAIN) {
 		fprintf(stderr, "recvmsg() failed: %s\n", strerr(errno));
 		return -1;
 	}
-
-	/* get timestamps of the received packet */
 	printstamps(&msg);
-	//struct cmsghdr *cm;
-	//for (cm = CMSG_FIRSTHDR(msg); cm; cm = CMSG_NXTHDR(msg, cm)) {
 
 	return ret;
 }
