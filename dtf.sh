eeaa9c79b7bdf20a0dd56b4b2e41a64aa6cb8072
kyechou <kuanyenchou@gmail.com>
2019-01-21 01:17:55 -0600

    Update script

--- dtf.sh
+++ dtf.sh
@@ -4,13 +4,15 @@ SCRIPT_DIR="$(dirname $(realpath ${BASH_SOURCE[0]}))"
 cd "$SCRIPT_DIR/tfbench/scripts/tf_cnn_benchmarks"
 
 python tf_cnn_benchmarks.py \
+	--device=cpu \
 	--local_parameter_device=cpu \
 	--data_format=NHWC \
+	--num_intra_threads=0 \
 	--batch_size=64 \
 	--model=trivial \
 	--variable_update=distributed_replicated \
 	--ps_hosts=192.17.151.31:6000 \
-	--worker_hosts=128.174.240.171:6000,128.174.240.171:6001 \
+	--worker_hosts=128.174.240.171:6000 \
 	$@
 
 #	--job_name=ps \
