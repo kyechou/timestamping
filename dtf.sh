150c5dbefb03712470d776d7ba146ce1a9655db6
kyechou <kuanyenchou@gmail.com>
2019-01-21 02:21:53 -0600

    Use parameter server to update variables

--- dtf.sh
+++ dtf.sh
@@ -8,9 +8,9 @@ python tf_cnn_benchmarks.py \
 	--local_parameter_device=cpu \
 	--data_format=NHWC \
 	--num_intra_threads=0 \
-	--batch_size=64 \
+	--batch_size=128 \
 	--model=trivial \
-	--variable_update=distributed_replicated \
+	--variable_update=parameter_server \
 	--ps_hosts=192.17.151.31:6000 \
 	--worker_hosts=128.174.240.171:6000 \
 	$@
