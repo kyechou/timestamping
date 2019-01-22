aa19be17e3e819619e4ab1abda12d1ff265110fa
kyechou <kuanyenchou@gmail.com>
2019-01-21 18:42:08 -0600

    Use resnet50

--- dtf.sh
+++ dtf.sh
@@ -9,7 +9,7 @@ python tf_cnn_benchmarks.py \
 	--data_format=NHWC \
 	--num_intra_threads=0 \
 	--batch_size=128 \
-	--model=trivial \
+	--model=resnet50 \
 	--variable_update=parameter_server \
 	--ps_hosts=192.17.151.31:6000 \
 	--worker_hosts=128.174.240.171:6000 \
