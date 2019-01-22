dd3af0ecfd01dce9f4f8574ab2972ca55444b684
kyechou <kuanyenchou@gmail.com>
2019-01-22 10:08:16 -0600

    Relocate async source files

--- src/async_client2.cpp
+++ src/async_client2.cpp
@@ -123,12 +123,11 @@ private:
 
 int main(int argc, char **argv)
 {
-
-
-	// Instantiate the client. It requires a channel, out of which the actual RPCs
-	// are created. This channel models a connection to an endpoint (in this case,
-	// localhost at port 50051). We indicate that the channel isn't authenticated
-	// (use of InsecureChannelCredentials()).
+	// Instantiate the client. It requires a channel, out of which the
+	// actual RPCs are created. This channel models a connection to an
+	// endpoint (in this case, localhost at port 50051). We indicate that
+	// the channel isn't authenticated (use of
+	// InsecureChannelCredentials()).
 	GreeterClient greeter(grpc::CreateChannel(
 	                              "localhost:50051", grpc::InsecureChannelCredentials()));
 
--- src/async_server.cpp
+++ src/async_server.cpp
@@ -1,30 +1,10 @@
-/*
- *
- * Copyright 2015 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
-#include <memory>
 #include <iostream>
+#include <memory>
 #include <string>
 #include <thread>
-
 #include <grpcpp/grpcpp.h>
 #include <grpc/support/log.h>
-
-#include "helloworld.grpc.pb.h"
+#include "echo.grpc.pb.h"
 
 using grpc::Server;
 using grpc::ServerAsyncResponseWriter;
@@ -32,12 +12,8 @@ using grpc::ServerBuilder;
 using grpc::ServerContext;
 using grpc::ServerCompletionQueue;
 using grpc::Status;
-using helloworld::HelloRequest;
-using helloworld::HelloReply;
-using helloworld::Greeter;
 
-class ServerImpl final
-{
+class ServerImpl final {
 public:
 	~ServerImpl()
 	{
@@ -70,8 +46,7 @@ public:
 
 private:
 	// Class encompasing the state and logic needed to serve a request.
-	class CallData
-	{
+	class CallData {
 	public:
 		// Take in the "service" instance (in this case representing an asynchronous
 		// server) and the completion queue "cq" used for asynchronous communication
