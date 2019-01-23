6cf3597b64a6be8487cb1b83dceb9794d8b49d39
kyechou <kuanyenchou@gmail.com>
2019-01-23 13:45:21 -0600

    Fix password option

--- send-timestamps.sh
+++ send-timestamps.sh
@@ -5,4 +5,4 @@ set -e
 SCRIPT_DIR="$(dirname $(realpath ${BASH_SOURCE[0]}))"
 cd "$SCRIPT_DIR"
 
-cat tsdump.*.sql | mysql -h mustang01 -u demo -p password -D demo
+cat tsdump.*.sql | mysql -h mustang01 -u demo -ppassword -D demo
