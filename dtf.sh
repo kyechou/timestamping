070f2cecc2f5cf524f29b009c584028a4fa30932
kyechou <kuanyenchou@gmail.com>
2019-01-23 18:37:40 +0000

    Update script for mustang GPU machines

--- dtf.sh
+++ dtf.sh
@@ -6,16 +6,16 @@ SCRIPT_DIR="$(dirname $(realpath ${BASH_SOURCE[0]}))"
 cd "$SCRIPT_DIR"
 
 python tfbench/scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py \
-	--device=cpu \
-	--local_parameter_device=cpu \
-	--data_format=NHWC \
+	--device=gpu \
+	--local_parameter_device=gpu \
 	--num_intra_threads=0 \
 	--batch_size=128 \
 	--model=resnet50 \
 	--variable_update=parameter_server \
-	--ps_hosts=192.17.151.31:6000 \
-	--worker_hosts=128.174.240.171:6000 \
+	--ps_hosts=mustang03:6000 \
+	--worker_hosts=mustang04:6001 \
 	$@
 
+#	--data_format=NHWC \
 #	--job_name=ps \
 #	--task_index=0
